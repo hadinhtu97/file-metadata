35d450d1bab31f51da9e241d288ab1e590498bd4
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-10 10:42:01 +0700

    uploads file with multer packer

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 const app = express();
 const port = 3000;
@@ -10,6 +12,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use(express.static(__dirname + "/public"));
+
+app.post('/api/file', upload.single('fileUpload'), (req, res) => {
+    if (req.file === undefined) {
+        res.json("No file is selected!")
+    } else {
+        res.json({
+            name: req.file.originalname,
+            type: req.file.mimetype,
+            size: req.file.size
+        })
+    }
+})
 
 app.listen(port, () => {
     console.log('Your app is listening on port ' + port);
