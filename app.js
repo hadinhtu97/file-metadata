6f62c3f75fc163e61a91bf6fa43d06b557ff8f69
Tu Ha <hadinhtu97@gmail.com>
2020-12-10 03:45:18 +0000

    rename input field name attribute

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use(express.static(__dirname + "/public"));
 
-app.post('/api/file', upload.single('fileUpload'), (req, res) => {
-    if (req.file === undefined) {
-        res.json("No file is selected!")
-    } else {
-        res.json({
-            name: req.file.originalname,
-            type: req.file.mimetype,
-            size: req.file.size
-        })
-    }
+app.post('/api/file', upload.single('upfile'), (req, res) => {
+  if (req.file === undefined) {
+    res.json("No file is selected!")
+  } else {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    })
+  }
 })
 
 app.listen(port, () => {
-    console.log('Your app is listening on port ' + port);
+  console.log('Your app is listening on port ' + port);
 })
