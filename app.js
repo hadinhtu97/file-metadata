1566f43c5ae52f78b880f44200d6796b160a54e8
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-10 09:45:21 +0700

    skeleton, start project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
+app.use(express.static(__dirname + "/public"));
+
+app.listen(port, () => {
+    console.log('Your app is listening on port ' + port);
+})
